ae4850a30d60b08cc1d51167118fa870bba3b7c5
leo guanco <leo guanco>
2017-04-08 20:34:59 -0300

    Actualizacion de componente para la reproduccion del audio

--- src/components/urban-player/urban-player.ts
+++ src/components/urban-player/urban-player.ts
@@ -1,4 +1,4 @@
-import { Component,Input } from '@angular/core';
+import { Component,Input,OnInit } from '@angular/core';
 
 /*
   Generated class for the UrbanPlayer component.
@@ -10,11 +10,53 @@ import { Component,Input } from '@angular/core';
   selector: 'urban-player',
   templateUrl: 'urban-player.html'
 })
-export class UrbanPlayerComponent {
-  @Input() text : string;
+export class UrbanPlayerComponent implements OnInit{  
+  @Input() title : string;
+  @Input() artist : string;
+  @Input() album : string;
+  @Input() cover : string;
+  @Input() audioSrc : string;
+
+  public audio:         any;
+  public ready:         boolean;
+  public playing:       boolean;
+  public audioPos:      number;
+  public audioDuration: number;
 
   constructor() {
-    console.log(this.text);    
+    this.ready = false;
+    this.playing = false;
+    this.audioPos = 0;
+  }
+
+  ngOnInit(){
+    this.audio = new Audio();
+    this.audio.src = this.audioSrc;
+    this.audio.load();
+
+    this.audio.oncanplaythrough = () => {
+      this.ready = true;
+      this.audioDuration = this.audio.duration;
+    };
+
+    this.audio.ontimeupdate = (event) => {
+      this.audioPos = this.audio.currentTime;
+    };
+
+    this.audio.onended = () => {
+      this.audioPos = 0;
+      this.playing = false;
+    }
+  }
+
+  togglePlay(){
+    if(!this.playing){
+      this.audio.play();
+    } else{
+      this.audio.pause();
+    }
+
+    this.playing = !this.playing;
   }
 
 }
